24ae9b657a3e1aafbb73c81b5a0582c8e1e0a723
Jbriguel <70860884+Jbriguel@users.noreply.github.com>
2025-01-06 18:47:52 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
   ],
-  base: '/kikiweb_test/'
+  base: '/kikiweb_test/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
